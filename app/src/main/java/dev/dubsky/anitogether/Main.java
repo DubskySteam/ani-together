c7a5fee6b65976a7ea3003846dda78cbdf6af140
Dubsky <23001099+DubskySteam@users.noreply.github.com>
2025-01-17 23:43:47 +0100

    main: updated to use all the new updates

--- app/src/main/java/dev/dubsky/anitogether/Main.java
+++ app/src/main/java/dev/dubsky/anitogether/Main.java
@@ -1,44 +1,51 @@
 package dev.dubsky.anitogether;
 
-import dev.dubsky.anitogether.network.HostStream;
-import dev.dubsky.anitogether.network.JoinStream;
-import dev.dubsky.anitogether.player.MpvController;
+import java.io.IOException;
+
+import dev.dubsky.anitogether.anime.AnimeWatchService;
+import dev.dubsky.anitogether.config.ConfigManager;
+import dev.dubsky.anitogether.network.Client;
+import dev.dubsky.anitogether.network.Host;
 import dev.dubsky.anitogether.ui.Menu;
 import dev.dubsky.anitogether.ui.MenuManager;
 import dev.dubsky.anitogether.util.StreamUtils;
 
 public class Main {
 
-    public static MpvController mpv;
-
+    public static ConfigManager configManager;
     public static void main(String[] args) {
-        mpv = new MpvController();
+
+        configManager = new ConfigManager();
+        
+        try {
+            configManager.loadConfig();
+            System.out.println("Config loaded successfully.");
+        } catch (IOException e) {
+            System.err.println("Error handling configuration: " + e.getMessage());
+        }
 
         MenuManager mainMenu = new MenuManager("AniTogether Main Menu");
 
-        mainMenu.addOption("1", "Watch Anime", choice -> {});
-        mainMenu.addOption("2", "Host Stream", choice -> HostStream.start());
+        mainMenu.addOption("1", "Watch Anime", choice -> AnimeWatchService.watchAnime());
+        mainMenu.addOption("2", "Host Stream", choice -> {
+            Host.getInstance().start();
+        });
         mainMenu.addOption("3", "Join Stream", choice -> {
-            String hostIp = getHostIp();
-            JoinStream.start(hostIp);
+            Client.getInstance().start();
         });
-        mainMenu.addOption("4", "Exit", choice -> {
-            StreamUtils.cleanup();
-            System.exit(0);
-        });
+        mainMenu.addOption("4", "Settings", choice -> Menu.showSettingsMenu());
+        mainMenu.addOption("5", "Exit", choice -> exit());
 
         while (true) {
-            Menu.clearMenu();
+            // Menu.clearMenu();
             mainMenu.display();
             String choice = mainMenu.getUserInput();
             mainMenu.executeChoice(choice);
         }
     }
 
-    private static String getHostIp() {
-        System.out.print("Enter the host's IP address: ");
-        try (java.util.Scanner scanner = new java.util.Scanner(System.in)) {
-            return scanner.nextLine().trim();
-        }
+    private static void exit() {
+        StreamUtils.cleanup();
+        System.exit(0);
     }
 }
